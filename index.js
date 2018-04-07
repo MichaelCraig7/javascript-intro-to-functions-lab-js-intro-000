027c9f3e4e4788c95d36fc5bb5a01b358a031784
Michael Craig <m_craig@baylor.edu>
2018-04-07 16:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log("HELLO".toLowerCase);
+  console.log("HELLO".toLowerCase)
 }
 
 function logWhisper(string) {
-  console.log("hello".toUpperCase);
+  console.log("hello".toUpperCase)
 }
